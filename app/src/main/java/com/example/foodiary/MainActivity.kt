d6402572c776bd6d6f694bbd53d7c8e31eecfc3a
study-with-HONG <hsilver084@naver.com>
2022-03-31 00:24:48 +0900

    저장소 삭제 후 다시 업로드...ㅠㅠㅠㅠㅠㅠ

--- app/src/main/java/com/example/foodiary/MainActivity.kt
+++ app/src/main/java/com/example/foodiary/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.foodiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moveHome = findViewById<ViewGroup>(R.id.mainView)
+        moveHome.setOnClickListener {
+            val itt = Intent(this, HomeActivity::class.java)
+            startActivity(itt)
+        }
     }
 }
